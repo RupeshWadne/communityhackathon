fc94438c89bccacd1e4ec8c069a5ce193a9ce587
shubham_wadne <kaylekashubham@gmail.com>
2023-07-16 18:50:44 +0530

    fixed error

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images : {
-        domains : [
-            'localhost', 
-            'https://bafybeidn2u3cj6nby4k4m6xpmcreefonaz5igcwfhylsnsatgthyulcaom.gateway.ipfscdn.io/0.jpg'
-        ]
-      }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
